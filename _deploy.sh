055c306d4e7e8e5ccf3311464668380c16097766
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-04-16 07:06:45 -0700

    Update attributation.

--- _deploy.sh
+++ _deploy.sh
@@ -5,11 +5,11 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "ernest.lee@chibifire.com"
+git config --global user.name "K. S. Ernest (iFire) Lee"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output
+cd Documentation
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
